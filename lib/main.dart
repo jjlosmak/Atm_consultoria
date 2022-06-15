2e200614306aebb2a63e2c38ed923e0348140c3c
FlutLab.io <>
2022-06-15 13:19:37 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:atmconsultoria/PaginaPrincipal.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: PaginaPrincipal(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
